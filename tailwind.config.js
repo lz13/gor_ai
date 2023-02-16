333e9050345e3de5521b2b1d42bb209fa18b5493
lz13 <lovro.zelic@icloud.com>
2023-02-16 16:37:26 +0100

    Add screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        'black': '#0d0c0b',
+        'zinc-white': '#fafafa'
+      }
+    },
   },
   plugins: [],
 }
